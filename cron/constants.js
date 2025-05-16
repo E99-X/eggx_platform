6a6fe3d9403c053aed1f64cc2a7cd9da5e39d985
NadyaL <nadya@devery.io>
2025-05-16 19:28:57 +0300

    add frontend eggx platform, cron job, setup deployment with firebase

---
+++ cron/constants.js
@@ -0,0 +1,6 @@
+export const SUI_RPC_URL = "https://fullnode.testnet.sui.io";
+export const AUTOPILOT_REGISTRY_ID =
+  "0xb330c1f81a15bbf76b8c41906caa42808229be75d959b03335de5dfa27e0d5ad";
+export const AUTO_PACKAGE_ID =
+  "0xd884f7edb4b8b2fcacccc256673901d095873471bc95c1b1e9f9f0748ce28af2";
+export const DRY_RUN = false;
